8a500671c52e5cd3044220aab31831a803cf71ed
ZNilakshi <nilakshisamarasekara0@gmail.com>
2025-04-25 00:44:35 +0530

    server mongo

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,8 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-// MongoDB Atlas connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log('MongoDB connection error:', err));
 
